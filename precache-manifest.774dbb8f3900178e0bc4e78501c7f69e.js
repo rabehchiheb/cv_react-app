b0adf69c43fa133c81659f9a43d5f0c668542074
chiheb <chiheb.rabeh@gmail.com>
2019-06-10 23:38:09 +0100

    Updates

---
+++ precache-manifest.774dbb8f3900178e0bc4e78501c7f69e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857c3088fb2a7c433ddd37b1d370cad6",
+    "url": "/cv_react-app/index.html"
+  },
+  {
+    "revision": "53110cd563cd36d55f77",
+    "url": "/cv_react-app/static/css/main.61017314.chunk.css"
+  },
+  {
+    "revision": "bbf7f360499be5fc0af6",
+    "url": "/cv_react-app/static/js/2.1de47be2.chunk.js"
+  },
+  {
+    "revision": "53110cd563cd36d55f77",
+    "url": "/cv_react-app/static/js/main.c515f0d5.chunk.js"
+  },
+  {
+    "revision": "e309a5b96e0ef79c4a79",
+    "url": "/cv_react-app/static/js/runtime~main.7688ccd8.js"
+  },
+  {
+    "revision": "0157a6f1221571058396ebe185b72faa",
+    "url": "/cv_react-app/static/media/naymar.0157a6f1.jpg"
+  }
+]);
